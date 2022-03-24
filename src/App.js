2debbd8cb06d09e6c28256c48f19da50670eb37c
Bugra Yuksel <bgryks18@gmail.com>
2022-03-24 10:39:15 +0300

    stiller düzenlendi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react'
-
+import React, { useRef } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap'
+import './style/style.min.css'
 const App = () => {
-  return <div>App</div>
+  return (
+    <div className="keyboard">
+      <div className="key white"></div>
+      <div className="key black"></div>
+      <div className="key white"></div>
+      <div className="key black"></div>
+      <div className="key white"></div>
+      <div className="key white"></div>
+      <div className="key black"></div>
+      <div className="key white"></div>
+      <div className="key black"></div>
+      <div className="key white"></div>
+      <div className="key black"></div>
+      <div className="key white"></div>
+    </div>
+  )
 }
 
 export default App
