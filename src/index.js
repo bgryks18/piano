e428de6d0fcd73c89b3ec5998827469d336d891d
Bugra Yuksel <bgryks18@gmail.com>
2022-03-28 12:13:17 +0300

    context providerı indexte kullanıldı

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-
-ReactDOM.render(<App />, document.getElementById('root'))
+import { PianoProvider } from './context/PianoContext'
+ReactDOM.render(
+  <PianoProvider>
+    <App />
+  </PianoProvider>,
+  document.getElementById('root'),
+)
